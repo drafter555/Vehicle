f928b649e0988169d185e8b4f0c641e5c972d851
drafter555 <101604435+drafter555@users.noreply.github.com>
2022-09-22 17:35:41 +0300

    car,ship,plane added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,24 @@
-﻿public abstract class Vehicle
+﻿
+using System.Threading.Channels;
+
+public static void Main(string[] args)
 {
-    public abstract void Go();
+public  abstract class Vehicle
+
+ { 
+    public abstract string Go();
+ }
+
+ public class Car : Vehicle
+ {
+    public override string Go()=>"Бип!";
+ }
+ public class Ship : Vehicle
+ {
+    public override string Go()=>"ТУУУУ!";
+ }
+ public class Plane : Vehicle
+ {
+    public override string Go()=>"Ууууу!";
+ }
 }
