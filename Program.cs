ee414fbb2875bc8bea63378ecc7f5c7f78e4d355
drafter555 <101604435+drafter555@users.noreply.github.com>
2022-09-22 18:03:02 +0300

    app done

--- Program.cs
+++ Program.cs
@@ -1,24 +1,37 @@
-﻿
-using System.Threading.Channels;
+﻿using System;
 
-public static void Main(string[] args)
+namespace DzmitryKryvenki
 {
-public  abstract class Vehicle
+ public static class Program
+ {
+   public static void Main(string[] args)
+  {
+   var _ferrari = new Car();
+   var _titanik = new Ship();
+   var _boeing = new Plane();
 
- { 
-    public abstract string Go();
- }
+   Console.WriteLine(_ferrari.Go());
+   Console.WriteLine(_titanik.Go());
+   Console.WriteLine(_boeing.Go());
+  }
+   public abstract class Vehicle
+  {
+   public abstract string Go();
+  }
 
- public class Car : Vehicle
- {
-    public override string Go()=>"Бип!";
- }
- public class Ship : Vehicle
- {
-    public override string Go()=>"ТУУУУ!";
- }
- public class Plane : Vehicle
- {
-    public override string Go()=>"Ууууу!";
+  public class Car : Vehicle
+  {
+   public override string Go() => "Бип!";
+  }
+
+  public class Ship : Vehicle
+  {
+   public override string Go() => "ТУУУУ!";
+  }
+
+  public class Plane : Vehicle
+  {
+   public override string Go() => "Ууууу!";
+  }
  }
 }
